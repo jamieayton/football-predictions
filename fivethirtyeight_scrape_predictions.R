6ed0137b1e91c0ccb8863da811f3b921a91d9369
Jamie Ayton <jamieayton@live.co.uk>
2017-06-15 08:05:44 +0100

    added a get_results function

--- fivethirtyeight_scrape_predictions.R
+++ fivethirtyeight_scrape_predictions.R
@@ -59,7 +59,7 @@ get_predictions <- function(url){
     home_prob = as.numeric(gsub("*%*<*", "", home_prob))/100,
     draw_prob = as.numeric(gsub("*%*<*", "", draw_prob))/100,
     away_prob = as.numeric(gsub("*%*<*", "", away_prob))/100,
-    scrape_datetime = Sys.time()
+    scrape_datetime = as.character(Sys.time())
   )
   
   # return
@@ -69,3 +69,88 @@ get_predictions <- function(url){
 
 
 
+
+
+get_results <- function(url){
+  # webpage
+  Sys.sleep(2.5+1*runif(1))
+  webpage <- read_html(url)
+  
+  # date
+  match_date <- webpage %>% 
+    html_nodes(css="#matches-table-wrapper > div.games-container.completed > div > table > tbody > tr.match-top > td.date") %>%
+    html_text()
+  
+  # teams
+  hometeam <- webpage %>% 
+    html_nodes(css="#matches-table-wrapper > div.games-container.completed > div > table > tbody > tr.match-top > td.team") %>% 
+    html_attr('data-str')
+  
+  awayteam <- webpage %>% 
+    html_nodes(css="#matches-table-wrapper > div.games-container.completed > div > table > tbody > tr.match-bottom > td.team") %>% 
+    html_attr('data-str')
+  
+  # probabilities
+  home_prob <- webpage %>% 
+    html_nodes(css="#matches-table-wrapper > div.games-container.completed > div > table > tbody > tr.match-top > td.prob") %>% 
+    html_text()
+  draw_prob <- webpage %>% 
+    html_nodes(css="#matches-table-wrapper > div.games-container.completed > div > table > tbody > tr.match-top > td.prob.tie-prob") %>% 
+    html_text()
+  away_prob <- webpage %>% 
+    html_nodes(css="#matches-table-wrapper > div.games-container.completed > div > table > tbody > tr.match-bottom > td.prob") %>% 
+    html_text()
+  
+  # adjust 'home_prob'
+  home_prob <- home_prob[seq(1, length(home_prob), by=2)]
+  
+  
+  # goal models
+  AdjG_home <- webpage %>% 
+    html_nodes(css="#matches-table-wrapper > div.games-container.completed > div") %>% 
+    html_nodes(css="div.additional-info-container > table > tbody > tr:nth-child(2) > td:nth-child(2)") %>% 
+    html_text()
+  AdjG_away <- webpage %>% 
+    html_nodes(css="#matches-table-wrapper > div.games-container.completed > div") %>% 
+    html_nodes(css="div.additional-info-container > table > tbody > tr:nth-child(2) > td:nth-child(3)") %>% 
+    html_text()
+  
+  ShotExG_home <- webpage %>% 
+    html_nodes(css="#matches-table-wrapper > div.games-container.completed > div") %>% 
+    html_nodes(css="div.additional-info-container > table > tbody > tr:nth-child(3) > td:nth-child(2)") %>% 
+    html_text()
+  ShotExG_away <- webpage %>% 
+    html_nodes(css="#matches-table-wrapper > div.games-container.completed > div") %>% 
+    html_nodes(css="div.additional-info-container > table > tbody > tr:nth-child(3) > td:nth-child(3)") %>% 
+    html_text()
+  
+  NonShotExG_home <- webpage %>% 
+    html_nodes(css="#matches-table-wrapper > div.games-container.completed > div") %>% 
+    html_nodes(css="div.additional-info-container > table > tbody > tr.moves > td:nth-child(2)") %>% 
+    html_text()
+  NonShotExG_away <- webpage %>% 
+    html_nodes(css="#matches-table-wrapper > div.games-container.completed > div") %>% 
+    html_nodes(css="div.additional-info-container > table > tbody > tr.moves > td:nth-child(3)") %>% 
+    html_text()
+  
+  
+  # clean and combine to dataframe
+  previous_results <- tibble(
+    match_date, 
+    hometeam, 
+    awayteam,home_prob = as.numeric(gsub("*%*<*", "", home_prob))/100, 
+    draw_prob = as.numeric(gsub("*%*<*", "", draw_prob))/100, 
+    away_prob = as.numeric(gsub("*%*<*", "", away_prob))/100,
+    AdjG_home = as.numeric(AdjG_home), 
+    AdjG_away = as.numeric(AdjG_away), 
+    ShotExG_home = as.numeric(ShotExG_home), 
+    ShotExG_away = as.numeric(ShotExG_away), 
+    NonShotExG_home = as.numeric(NonShotExG_home), 
+    NonShotExG_away = as.numeric(NonShotExG_away), 
+    scrape_datetime = as.character(Sys.time()) 
+  )
+  
+  # return
+  previous_results
+  
+}
