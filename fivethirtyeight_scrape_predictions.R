cc71753bc9f4d6da7e287b83bec2ceb04aa3acb9
Jamie Ayton <jamieayton@live.co.uk>
2017-06-15 08:41:26 +0100

    adjusted get functions to deal with 'no data' situations better

--- fivethirtyeight_scrape_predictions.R
+++ fivethirtyeight_scrape_predictions.R
@@ -51,16 +51,39 @@ get_upcoming_games <- function(url){
   # adjust 'home_prob'
   home_prob <- home_prob[seq_along(home_prob) %% 2 == 1]
   
-  # clean and combine to dataframe
-  predictions <- tibble(
-    match_date,
-    hometeam,
-    awayteam,
-    home_prob = as.numeric(gsub("*%*<*", "", home_prob))/100,
-    draw_prob = as.numeric(gsub("*%*<*", "", draw_prob))/100,
-    away_prob = as.numeric(gsub("*%*<*", "", away_prob))/100,
-    scrape_datetime = as.character(Sys.time())
+  
+  # check existance of data
+  check_existance <- all(
+    length(match_date) > 1, length(hometeam) > 1, length(awayteam) > 1, length(home_prob) > 1, length(draw_prob) > 1, length(away_prob) > 1
   )
+  
+  if(check_existance == TRUE){
+    
+    # clean and combine to dataframe
+    predictions <- tibble(
+      match_date,
+      hometeam,
+      awayteam,
+      home_prob = as.numeric(gsub("*%*<*", "", home_prob))/100,
+      draw_prob = as.numeric(gsub("*%*<*", "", draw_prob))/100,
+      away_prob = as.numeric(gsub("*%*<*", "", away_prob))/100,
+      scrape_datetime = as.character(Sys.time())
+    )
+    
+  } else {
+    
+    # output empty tibble
+    predictions <- tibble(
+      match_date = NA,
+      hometeam = NA,
+      awayteam = NA,
+      home_prob = NA,
+      draw_prob = NA,
+      away_prob = NA,
+      scrape_datetime = as.character(Sys.time())
+    )
+  }
+  
   
   # return
   predictions
@@ -134,21 +157,50 @@ get_completed_games <- function(url){
     html_text()
   
   
-  # clean and combine to dataframe
-  previous_results <- tibble(
-    match_date, 
-    hometeam, 
-    awayteam,home_prob = as.numeric(gsub("*%*<*", "", home_prob))/100, 
-    draw_prob = as.numeric(gsub("*%*<*", "", draw_prob))/100, 
-    away_prob = as.numeric(gsub("*%*<*", "", away_prob))/100,
-    AdjG_home = as.numeric(AdjG_home), 
-    AdjG_away = as.numeric(AdjG_away), 
-    ShotExG_home = as.numeric(ShotExG_home), 
-    ShotExG_away = as.numeric(ShotExG_away), 
-    NonShotExG_home = as.numeric(NonShotExG_home), 
-    NonShotExG_away = as.numeric(NonShotExG_away), 
-    scrape_datetime = as.character(Sys.time()) 
+  # check existance of data
+  check_existance <- all(
+    length(match_date) > 1, length(hometeam) > 1, length(awayteam) > 1, length(home_prob) > 1, length(draw_prob) > 1, length(away_prob) > 1
   )
+  
+  if(check_existance == TRUE){
+    
+    # clean and combine to dataframe
+    previous_results <- tibble(
+      match_date, 
+      hometeam, 
+      awayteam,
+      home_prob = as.numeric(gsub("*%*<*", "", home_prob))/100, 
+      draw_prob = as.numeric(gsub("*%*<*", "", draw_prob))/100, 
+      away_prob = as.numeric(gsub("*%*<*", "", away_prob))/100,
+      AdjG_home = as.numeric(AdjG_home), 
+      AdjG_away = as.numeric(AdjG_away), 
+      ShotExG_home = as.numeric(ShotExG_home), 
+      ShotExG_away = as.numeric(ShotExG_away), 
+      NonShotExG_home = as.numeric(NonShotExG_home), 
+      NonShotExG_away = as.numeric(NonShotExG_away), 
+      scrape_datetime = as.character(Sys.time()) 
+    )
+    
+  } else {
+    
+    # output empty tibble
+    previous_results <- tibble(
+      match_date = NA, 
+      hometeam = NA, 
+      awayteam = NA,
+      home_prob = NA, 
+      draw_prob = NA, 
+      away_prob = NA, 
+      AdjG_home = NA, 
+      AdjG_away = NA, 
+      ShotExG_home = NA, 
+      ShotExG_away = NA, 
+      NonShotExG_home = NA, 
+      NonShotExG_away = NA, 
+      scrape_datetime = as.character(Sys.time()) 
+    )
+    
+  }
   
   # return
   previous_results
@@ -169,7 +221,8 @@ urls_list <- tibble(
 )
 
 
-urls_list
+upcoming_games <- map(urls_list$url, get_upcoming_games)
+completed_games <- map(urls_list$url, get_completed_games)
 
 
 
