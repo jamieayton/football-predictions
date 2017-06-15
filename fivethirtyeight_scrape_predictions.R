7309b6996bd1006085b925d45faa007b0c8eed20
Jamie Ayton <jamieayton@live.co.uk>
2017-06-15 07:51:17 +0100

    init

---
+++ fivethirtyeight_scrape_predictions.R
@@ -0,0 +1,21 @@
+
+
+
+# 1. Setup ----------------------------------------------------------------
+
+# global options
+options(stringsAsFactors = FALSE)
+
+# packages
+library('tidyverse')
+library('rvest')
+
+# dir
+project_wd <- getwd()
+
+
+
+
+
+
+
