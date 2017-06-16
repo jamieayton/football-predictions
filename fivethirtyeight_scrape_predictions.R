6041e5075d63685818b3b8a96da1d427a34afde4
Jamie Ayton <jamieayton@live.co.uk>
2017-06-16 06:05:48 +0100

    improved layout of get functions for no data situations

--- fivethirtyeight_scrape_predictions.R
+++ fivethirtyeight_scrape_predictions.R
@@ -27,6 +27,22 @@ get_upcoming_games <- function(url){
   # base_css
   base_css <- webpage %>% html_nodes(css="#matches-table-wrapper > div.games-container.upcoming > div")
   
+  if (length(base_css) == 0){
+    
+    # output empty tibble
+    predictions <- tibble(
+      stage = NA, 
+      match_date = NA,
+      hometeam = NA,
+      awayteam = NA,
+      home_prob = NA,
+      draw_prob = NA,
+      away_prob = NA,
+      scrape_datetime = as.character(Sys.time())
+    )
+    
+  } else {
+  
   # nodes to map through
   css_i <- paste0("#matches-table-wrapper > div.games-container.upcoming > div:nth-child(", seq_along(base_css), ")")
   
@@ -76,48 +92,28 @@ get_upcoming_games <- function(url){
       html_text() %>% ifelse(length(.) == 0, NA, .)
   )
   
-
   
-  # check existance of data
-  check_existance <- all(
-    length(match_date) > 1, length(hometeam) > 1, length(awayteam) > 1, length(home_prob) > 1, length(draw_prob) > 1, length(away_prob) > 1
+  
+  # clean and combine to dataframe
+  predictions <- tibble(
+    stage,
+    match_date,
+    hometeam,
+    awayteam,
+    home_prob = as.numeric(gsub("*%*<*", "", home_prob))/100,
+    draw_prob = as.numeric(gsub("*%*<*", "", draw_prob))/100,
+    away_prob = as.numeric(gsub("*%*<*", "", away_prob))/100,
+    scrape_datetime = as.character(Sys.time())
   )
   
-  if(check_existance == TRUE){
-    
-    # clean and combine to dataframe
-    predictions <- tibble(
-      stage,
-      match_date,
-      hometeam,
-      awayteam,
-      home_prob = as.numeric(gsub("*%*<*", "", home_prob))/100,
-      draw_prob = as.numeric(gsub("*%*<*", "", draw_prob))/100,
-      away_prob = as.numeric(gsub("*%*<*", "", away_prob))/100,
-      scrape_datetime = as.character(Sys.time())
-    )
-    
-    # apply stage to rows below
-    for (i in seq(1, nrow(previous_results))){
-      previous_results$stage[i] <- ifelse(is.na(previous_results$stage[i]) & i > 1, previous_results$stage[i-1], previous_results$stage[i])
-    }
-    
-    # drop NAs
-    previous_results <- previous_results %>% drop_na()
-    
-  } else {
-    
-    # output empty tibble
-    predictions <- tibble(
-      stage = NA, 
-      match_date = NA,
-      hometeam = NA,
-      awayteam = NA,
-      home_prob = NA,
-      draw_prob = NA,
-      away_prob = NA,
-      scrape_datetime = as.character(Sys.time())
-    )
+  # apply stage to rows below
+  for (i in seq(1, nrow(predictions))){
+    predictions$stage[i] <- ifelse(is.na(predictions$stage[i]) & i > 1, predictions$stage[i-1], predictions$stage[i])
+  }
+  
+  # drop NAs
+  predictions <- predictions %>% drop_na()
+
   }
   
   
@@ -138,94 +134,110 @@ get_completed_games <- function(url){
   # base_css
   base_css <- webpage %>% html_nodes(css="#matches-table-wrapper > div.games-container.completed > div")
   
-  # nodes to map through
-  css_i <- paste0("#matches-table-wrapper > div.games-container.completed > div:nth-child(", seq_along(base_css), ")")
-  
-  # stage
-  stage <- map_chr(
-    css_i, 
-    function(x) if(webpage %>% html_nodes(css=x) %>% html_attr('class') %>% grepl("match-container", .)){
-      NA
-    } else {
-      webpage %>% html_nodes(css=x) %>% html_text()
-    }
-  )
-  
-  # date
-  match_date <- map_chr(
-    css_i, 
-    function(x) webpage %>% html_nodes(css=x) %>% html_nodes(css="table > tbody > tr.match-top > td.date") %>% 
-      html_text() %>% ifelse(length(.) == 0, NA, .)
-  )
-  
-  # teams
-  hometeam <- map_chr(
-    css_i, 
-    function(x) webpage %>% html_nodes(css=x) %>% html_nodes(css="table > tbody > tr.match-top > td.team") %>% 
-      html_attr('data-str') %>% ifelse(length(.) == 0, NA, .)
-  )
-  awayteam <- map_chr(
-    css_i, 
-    function(x) webpage %>% html_nodes(css=x) %>% html_nodes(css="table > tbody > tr.match-bottom > td.team") %>% 
-    html_attr('data-str') %>% ifelse(length(.) == 0, NA, .)
-  )
-  
-  # probabilities
-  home_prob <- map_chr(
-    css_i, 
-    function(x) webpage %>% html_nodes(css=x) %>% html_nodes(css="table > tbody > tr.match-top > td.prob") %>% 
-      html_text() %>% ifelse(length(.) == 0, NA, .)
-  )
-  draw_prob <- map_chr(
-    css_i, 
-    function(x) webpage %>% html_nodes(css=x) %>% html_nodes(css="table > tbody > tr.match-top > td.prob.tie-prob") %>% 
-      html_text() %>% ifelse(length(.) == 0, NA, .)
-  )
-  away_prob <- map_chr(
-    css_i, 
-    function(x) webpage %>% html_nodes(css=x) %>% html_nodes(css="tbody > tr.match-bottom > td.prob") %>% 
-      html_text() %>% ifelse(length(.) == 0, NA, .)
-  )
-  
-  # goal models
-  AdjG_home <- map_chr(
-    css_i, 
-    function(x) webpage %>% html_nodes(css=x) %>% html_nodes(css="div.additional-info-container > table > tbody > tr:nth-child(2) > td:nth-child(2)") %>% 
-      html_text() %>% ifelse(length(.) == 0, NA, .)
-  )
-  AdjG_away <- map_chr(
-    css_i, 
-    function(x) webpage %>% html_nodes(css=x) %>% html_nodes(css="div.additional-info-container > table > tbody > tr:nth-child(2) > td:nth-child(3)") %>% 
-      html_text() %>% ifelse(length(.) == 0, NA, .)
-  )
-  ShotExG_home <- map_chr(
-    css_i, 
-    function(x) webpage %>% html_nodes(css=x) %>% html_nodes(css="div.additional-info-container > table > tbody > tr:nth-child(3) > td:nth-child(2)") %>% 
-      html_text() %>% ifelse(length(.) == 0, NA, .)
-  )
-  ShotExG_away <- map_chr(
-    css_i, 
-    function(x) webpage %>% html_nodes(css=x) %>% html_nodes(css="div.additional-info-container > table > tbody > tr:nth-child(3) > td:nth-child(3)") %>% 
-      html_text() %>% ifelse(length(.) == 0, NA, .)
-  )
-  NonShotExG_home <- map_chr(
-    css_i, 
-    function(x) webpage %>% html_nodes(css=x) %>% html_nodes(css="div.additional-info-container > table > tbody > tr.moves > td:nth-child(2)") %>% 
-      html_text() %>% ifelse(length(.) == 0, NA, .)
-  )
-  NonShotExG_away <- map_chr(
-    css_i, 
-    function(x) webpage %>% html_nodes(css=x) %>% html_nodes(css="div.additional-info-container > table > tbody > tr.moves > td:nth-child(3)") %>% 
-      html_text() %>% ifelse(length(.) == 0, NA, .)
-  )
+  if (length(base_css) == 0){
+    
+    # output empty tibble
+    previous_results <- tibble(
+      stage = NA, 
+      match_date = NA, 
+      hometeam = NA, 
+      awayteam = NA,
+      home_prob = NA, 
+      draw_prob = NA, 
+      away_prob = NA, 
+      AdjG_home = NA, 
+      AdjG_away = NA, 
+      ShotExG_home = NA, 
+      ShotExG_away = NA, 
+      NonShotExG_home = NA, 
+      NonShotExG_away = NA, 
+      scrape_datetime = as.character(Sys.time())
+    )
+    
+    
+  } else {
+    
+    # nodes to map through
+    css_i <- paste0("#matches-table-wrapper > div.games-container.completed > div:nth-child(", seq_along(base_css), ")")
+    
+    # stage
+    stage <- map_chr(
+      css_i, 
+      function(x) if(webpage %>% html_nodes(css=x) %>% html_attr('class') %>% grepl("match-container", .)){
+        NA
+      } else {
+        webpage %>% html_nodes(css=x) %>% html_text()
+      }
+    )
+    
+    # date
+    match_date <- map_chr(
+      css_i, 
+      function(x) webpage %>% html_nodes(css=x) %>% html_nodes(css="table > tbody > tr.match-top > td.date") %>% 
+        html_text() %>% ifelse(length(.) == 0, NA, .)
+    )
+    
+    # teams
+    hometeam <- map_chr(
+      css_i, 
+      function(x) webpage %>% html_nodes(css=x) %>% html_nodes(css="table > tbody > tr.match-top > td.team") %>% 
+        html_attr('data-str') %>% ifelse(length(.) == 0, NA, .)
+    )
+    awayteam <- map_chr(
+      css_i, 
+      function(x) webpage %>% html_nodes(css=x) %>% html_nodes(css="table > tbody > tr.match-bottom > td.team") %>% 
+        html_attr('data-str') %>% ifelse(length(.) == 0, NA, .)
+    )
+    
+    # probabilities
+    home_prob <- map_chr(
+      css_i, 
+      function(x) webpage %>% html_nodes(css=x) %>% html_nodes(css="table > tbody > tr.match-top > td.prob") %>% 
+        html_text() %>% ifelse(length(.) == 0, NA, .)
+    )
+    draw_prob <- map_chr(
+      css_i, 
+      function(x) webpage %>% html_nodes(css=x) %>% html_nodes(css="table > tbody > tr.match-top > td.prob.tie-prob") %>% 
+        html_text() %>% ifelse(length(.) == 0, NA, .)
+    )
+    away_prob <- map_chr(
+      css_i, 
+      function(x) webpage %>% html_nodes(css=x) %>% html_nodes(css="tbody > tr.match-bottom > td.prob") %>% 
+        html_text() %>% ifelse(length(.) == 0, NA, .)
+    )
+    
+    # goal models
+    AdjG_home <- map_chr(
+      css_i, 
+      function(x) webpage %>% html_nodes(css=x) %>% html_nodes(css="div.additional-info-container > table > tbody > tr:nth-child(2) > td:nth-child(2)") %>% 
+        html_text() %>% ifelse(length(.) == 0, NA, .)
+    )
+    AdjG_away <- map_chr(
+      css_i, 
+      function(x) webpage %>% html_nodes(css=x) %>% html_nodes(css="div.additional-info-container > table > tbody > tr:nth-child(2) > td:nth-child(3)") %>% 
+        html_text() %>% ifelse(length(.) == 0, NA, .)
+    )
+    ShotExG_home <- map_chr(
+      css_i, 
+      function(x) webpage %>% html_nodes(css=x) %>% html_nodes(css="div.additional-info-container > table > tbody > tr:nth-child(3) > td:nth-child(2)") %>% 
+        html_text() %>% ifelse(length(.) == 0, NA, .)
+    )
+    ShotExG_away <- map_chr(
+      css_i, 
+      function(x) webpage %>% html_nodes(css=x) %>% html_nodes(css="div.additional-info-container > table > tbody > tr:nth-child(3) > td:nth-child(3)") %>% 
+        html_text() %>% ifelse(length(.) == 0, NA, .)
+    )
+    NonShotExG_home <- map_chr(
+      css_i, 
+      function(x) webpage %>% html_nodes(css=x) %>% html_nodes(css="div.additional-info-container > table > tbody > tr.moves > td:nth-child(2)") %>% 
+        html_text() %>% ifelse(length(.) == 0, NA, .)
+    )
+    NonShotExG_away <- map_chr(
+      css_i, 
+      function(x) webpage %>% html_nodes(css=x) %>% html_nodes(css="div.additional-info-container > table > tbody > tr.moves > td:nth-child(3)") %>% 
+        html_text() %>% ifelse(length(.) == 0, NA, .)
+    )
 
-
-  # check existance of data
-  check_existance <- all(
-    length(match_date) > 1, length(hometeam) > 1, length(awayteam) > 1, length(home_prob) > 1, length(draw_prob) > 1, length(away_prob) > 1
-  )
-  
-  if(check_existance == TRUE){
     
     # clean and combine to dataframe
     previous_results <- tibble(
@@ -254,32 +266,13 @@ get_completed_games <- function(url){
     previous_results <- previous_results %>% drop_na()
     
     
-  } else {
-    
-    # output empty tibble
-    previous_results <- tibble(
-      stage = NA, 
-      match_date = NA, 
-      hometeam = NA, 
-      awayteam = NA,
-      home_prob = NA, 
-      draw_prob = NA, 
-      away_prob = NA, 
-      AdjG_home = NA, 
-      AdjG_away = NA, 
-      ShotExG_home = NA, 
-      ShotExG_away = NA, 
-      NonShotExG_home = NA, 
-      NonShotExG_away = NA, 
-      scrape_datetime = as.character(Sys.time()) 
-    )
-    
   }
   
   # return
   previous_results
   
 }
+
 
 
 
